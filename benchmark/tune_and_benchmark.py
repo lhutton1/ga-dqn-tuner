551f11e7182b0889331acfad763b52cb153667e8
Luke Hutton <luke.hutton1@outlook.com>
2020-10-31 07:44:50 -0400

    tune and benchmark script

---
+++ benchmark/tune_and_benchmark.py
@@ -0,0 +1,32 @@
+from tune_model import tune_model
+from benchmark_model import compile_model, run_model
+
+if __name__ == "__main__":
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        target_string = data['target']
+        tune_settings = data['autotuner_settings']
+
+        for model in data['models']:
+            trace, input_shapes = get_model(model['name'], model['type'])
+            mod, params = relay.frontend.from_pytorch(trace, input_shapes)
+            print(f"Tuning model {model['name']}")
+            tune_model(mod, params, tune_settings, target_string)
+
+	run_settings = data['run_settings']
+
+        for model in data['models']:
+            for apply_tuning in [True, False]:
+                trace, input_shapes = get_model(model['name'], model['type'])
+                mod, params = relay.frontend.from_pytorch(trace, input_shapes)
+
+                print(f"Compiling model {model['name']}")
+                if apply_tuning:
+                    graph, lib, params = compile_model(mod, params, target_string, tune_settings['tuning_records'])
+                else:
+                    graph, lib, params = compile_model(mod, params, target_string)
+                print(f"Running model {model['name']}")
+                if apply_tuning:
+                    run_model(graph, lib, params, run_settings, model['name'], tune_settings['tuning_records'])
+                else:
+                    run_model(graph, lib, params, run_settings, model['name'])
